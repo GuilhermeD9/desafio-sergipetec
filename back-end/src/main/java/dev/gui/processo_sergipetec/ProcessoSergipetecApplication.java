fa92b738850cd71bd0885d541ea61bf9a331d075
Guilherme Silva <sgsouza.173@gmail.com>
2025-01-20 15:19:51 -0300

    init: first commit

---
+++ back-end/src/main/java/dev/gui/processo_sergipetec/ProcessoSergipetecApplication.java
@@ -0,0 +1,13 @@
+package dev.gui.processo_sergipetec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessoSergipetecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessoSergipetecApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/dev/gui/processo_sergipetec/ProcessoSergipetecApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gui.processo_sergipetec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessoSergipetecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
