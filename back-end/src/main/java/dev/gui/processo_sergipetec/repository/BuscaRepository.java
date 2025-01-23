74feb32f8051d3c73c82654f2457e3c335712fae
Guilherme Silva <sgsouza.173@gmail.com>
2025-01-23 16:33:58 -0300

    feat: Nova classe repository

---
+++ back-end/src/main/java/dev/gui/processo_sergipetec/repository/BuscaRepository.java
@@ -0,0 +1,13 @@
+package dev.gui.processo_sergipetec.repository;
+
+import dev.gui.processo_sergipetec.model.VeiculoModel;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class BuscaRepository {
+
+    public List<VeiculoModel> consultarVeiculos(String tipo, String modelo, String cor, Integer ano) throws SQLException {
+
+    }
+}
