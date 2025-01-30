1363abd2c21df9604530c9bb0772064cfb34f71d
Guilherme Silva <sgsouza.173@gmail.com>
2025-01-29 22:56:17 -0300

    refactor: Remocao do lombok, evitando bugs

--- back-end/src/main/java/dev/gui/processo_sergipetec/model/VeiculoModel.java
+++ back-end/src/main/java/dev/gui/processo_sergipetec/model/VeiculoModel.java
@@ -1,12 +1,6 @@
 package dev.gui.processo_sergipetec.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class VeiculoModel {
     private int id;
     private String modelo;
@@ -15,4 +9,75 @@ public class VeiculoModel {
     private int ano;
     private double preco;
     private String tipo;
+
+    // Construtores, Getters e Setters
+
+    public VeiculoModel(int id, String modelo, String fabricante, String cor, int ano, double preco, String tipo) {
+        this.id = id;
+        this.modelo = modelo;
+        this.fabricante = fabricante;
+        this.cor = cor;
+        this.ano = ano;
+        this.preco = preco;
+        this.tipo = tipo;
+    }
+
+    public VeiculoModel() {
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
